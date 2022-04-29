27ab88be805f553e2aeb1eb3d1dc24a22edd2669
DarwinAndrade <andres.63.d@gmail.com>
2022-04-29 16:18:28 -0300

    [ADD][stock_reserve_heben] Incidence nbr./s.: ODOO-3105.  - Add fields in locations

---
+++ stock_reserve_heben/models/sale_order_line.py
@@ -0,0 +1,8 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    warehouses = fields.Many2one('stock.location', string=_("Warehouses"))
