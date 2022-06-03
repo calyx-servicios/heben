0a8d9403913cca68b5f4ee86be666a7cb15dc536
DarwinAndrade <andres.63.d@gmail.com>
2022-06-03 08:31:59 -0300

    [ADD][stock_picking_batch_extended_custom] Incidence nbr./s.: ODOO-3652  - Fix merge conflicts

--- stock_picking_batch_extended_custom/__manifest__.py
+++ stock_picking_batch_extended_custom/__manifest__.py
@@ -1,7 +1,3 @@
-# Copyright 2012-2014 Alexandre Fayolle, Camptocamp SA
-# Copyright 2018-2020 Tecnativa - Carlos Dauden
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
 {
     "name": "Stock batch picking Custom",
     "summary": "Add modifications to the module stock picking batch extended",
