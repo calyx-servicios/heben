c027e4345e4eb09cbbb7b5758266193d5c3ecc0c
cristian <crisdparadiso@gmail.com>
2022-10-06 16:27:13 -0300

    [ODOO-????][IMP][location_by_proximity]

--- location_by_proximity/models/stock_move.py
+++ location_by_proximity/models/stock_move.py
@@ -4,11 +4,12 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     def create(self, vals_list):
-        so = self.env['sale.order'].search([('name','=',vals_list[0]['origin'])])
-        if so:
-            so_lines = so.order_line
-            if so_lines:
-                for vals_line,so_line in zip(vals_list,so_lines):
-                    vals_line['location_id'] = so_line.location_id.id
-        res = super().create(vals_list)
-        return res
+        if vals_list[0].get('origin',False):
+            so = self.env['sale.order'].search([('name','=',vals_list[0]['origin'])])
+            if so:
+                so_lines = so.order_line
+                if so_lines:
+                    for vals_line,so_line in zip(vals_list,so_lines):
+                        vals_line['location_id'] = so_line.location_id.id
+             
+        return super().create(vals_list)
