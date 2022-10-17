dbdabd5890141d78b1eeea20e856076b12e6f2d0
cristian <crisdparadiso@gmail.com>
2022-10-17 13:53:39 -0300

    [ODOO-2372][ADD][product_sku]

--- product_SKU/models/__init__.py
+++ product_SKU/models/__init__.py
@@ -1,3 +1,4 @@
-from . import product_sku
-from . import product_attribute_value
-from . import product_template
+from . import mask_sku
+from . import product_attribute
+from . import product_product
+from . import sku_rule
