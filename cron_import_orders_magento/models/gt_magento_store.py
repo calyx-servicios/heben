b21180b2d290e6ab53eaa356874088a10c3adb0a
cristian <crisdparadiso@gmail.com>
2023-02-03 13:33:23 -0300

    [ODOO-5917][ADD][cron_import_orders_magento]

---
+++ cron_import_orders_magento/models/gt_magento_store.py
@@ -0,0 +1,10 @@
+from odoo import models
+
+class GTMagentoStore(models.Model):
+    _inherit = "gt.magento.store"
+
+    def import_ar_orders(self):
+        self.env['gt.magento.store'].search([('name','=','Heben AR View')]).GtCreateMagentoOrders()
+
+    def import_cl_orders(self):
+        self.env['gt.magento.store'].search([('name','=','Heben CL View')]).GtcreateMagentoOrders()
