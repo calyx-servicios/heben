bb3e78fe24041a31bac6752ed941b88813ccab3c
Garaceliguzman <georginaguzman267@gmail.com>
2023-02-22 16:57:00 -0300

    [ODOO-5410][FIX][ pos_style_improvements]

---
+++ pos_style_improvements/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Pos Style Improvements",
+    "summary": """
+        change in styles from pos""",
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["GeorginaGuzman"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Stock",
+    "version": "13.0.2.0.0",
+    "development_status": "Production/Stable",
+    "depends": ['point_of_sale'],
+    "data": [
+        'views/templates.xml'
+    ],
+}
