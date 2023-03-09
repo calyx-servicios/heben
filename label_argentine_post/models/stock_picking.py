551491fe04dbf45ec90ed94a60a1e3dde19f8c04
garaceliguzman <georginaguzman267@gmail.com>
2023-03-09 11:29:59 -0300

    [ODOO-6113][FIX][label_argentine_post]

--- label_argentine_post/models/stock_picking.py
+++ label_argentine_post/models/stock_picking.py
@@ -7,9 +7,12 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
 
-    in_branch = fields.Boolean(string="In Branch", default=False)
+    in_branch = fields.Selection([('in_branch','In Branch'),
+            ('in_address','In Address'),],string="Delivery type")
 
     branch_post_id = fields.Many2one('stock.post.branch',string='Branch Post')
+
+
 
     def create_template_label_in_branch(self):
         return self.env.ref('label_argentine_post.action_report_preprinted_label_in_branch').report_action(self)
