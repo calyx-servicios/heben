93a81b025fab1f7207037e0351d32e6b3fbd2cdd
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-07-27 15:41:26 -0300

    [ODOO-6995][UPD][internal_code_product] - Update on prod

---
+++ internal_code_product/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Internal code products",
+    "summary": """
+        This module show internal code on product templates
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["Zamora, javier"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "category": "Products",
+    "version": "13.0.1.0.0",
+    "application": False,
+    "installable": True,
+    "license": "AGPL-3",
+    "depends": ["product_internal_code"],
+    "data": [
+        "views/product_view.xml",
+    ],
+}
