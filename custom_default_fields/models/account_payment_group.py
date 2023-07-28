ef2838ec042e44d736b25866456cd560c13e4636
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-07-28 09:46:35 -0300

    [ODOO-7005][FIX][custom_default_fields] - Overrid onchage methods, add methods on manual field

--- custom_default_fields/models/account_payment_group.py
+++ custom_default_fields/models/account_payment_group.py
@@ -14,3 +14,27 @@ class AccountPaymentGroup(models.Model):
         states={'draft': [('readonly', False)],
                 'confirmed': [('readonly', False)]}
     )
+
+    @api.onchange('retencion_ganancias')
+    def on_change_retencion_ganancias(self):
+        if self.retencion_ganancias == 'no_aplica':
+            self.regimen_ganancias_id = False
+        elif self.retencion_ganancias == 'nro_regimen':
+            cia_regs = self.company_regimenes_ganancias_ids
+            partner_regimen = (
+                self.commercial_partner_id.default_regimen_ganancias_id)
+            if partner_regimen:
+                def_regimen = partner_regimen
+            elif cia_regs:
+                def_regimen = cia_regs[0]
+            else:
+                def_regimen = False
+            self.regimen_ganancias_id = def_regimen
+
+    @api.onchange('commercial_partner_id')
+    def change_retencion_ganancias(self):
+        pass
+
+    @api.onchange('company_regimenes_ganancias_ids')
+    def change_company_regimenes_ganancias(self):
+        pass
