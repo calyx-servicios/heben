1fc78b791ee5503623e9cc97b93a4652b3aedf72
gabbiiperez <gabbiiperez94@gmail.com>
2023-07-28 14:52:47 -0300

    [ODOO-7013][IMP][product_views_access] - IMP pricelist with product multi company.

---
+++ product_views_access/__manifest__.py
@@ -0,0 +1,24 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Product Views Access',
+    'summary': """
+        This module adds the permissions for product views.
+    """,
+    'author': 'Calyx Servicios S.A.',
+    'maintainers': ['PerezGabriela'],
+    'website': 'http://odoo.calyx-cloud.com.ar/',
+    'license': 'AGPL-3',
+    'category': 'Stock',
+    'version': '13.0.3.0.0',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'point_of_sale',
+        'website_sale'
+    ],
+    'data': [
+        'security/product_views_access_security.xml',
+        'views/product_views.xml',
+        'views/product_pricelist_item_view.xml'
+    ],
+}
