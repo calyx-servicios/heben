f95856520b591d127493f38df9dfb8a6d0acf8a4
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-08-01 10:57:37 -0300

    [ODOO-6664][IMP][magento_product_export] - Get ds_catg from new field

--- magento_product_export/models/product_product.py
+++ magento_product_export/models/product_product.py
@@ -7,7 +7,6 @@ class ProductProduct(models.Model):
     ATTRIBUTE_FIELD_MAPPING = {
         'ds_color_filter': 'ds_color_filter',
         'ds_material_filter': 'ds_material_filter',
-        'ds_category': 'ds_category',
     }
 
     def _check_magento_manager_permission(self):
@@ -18,6 +17,10 @@ class ProductProduct(models.Model):
         if not product.description:
             raise UserError(_('To export to Magento, the description field is required'))
 
+        ds_category = product.prod_attr_category_id.label
+        if not ds_category:
+            raise UserError(_('The product {} does not have ds_category'.format(product.name)))
+
         vals = {
             'sku': product.default_code or '',
             'name': product.name,
@@ -25,6 +28,7 @@ class ProductProduct(models.Model):
             'price': product.lst_price,
             'stock': product.qty_available,
             'meta_description': product.description,
+            'ds_category': ds_category,
         }
 
         return vals
@@ -37,15 +41,22 @@ class ProductProduct(models.Model):
             vals = self._prepare_magento_product_data(product)
 
             attributes_magento = product.product_template_attribute_value_ids.filtered(lambda x: x.attribute_id.is_attribute_magento)
-            if not attributes_magento:
-                raise UserError(_('The product {} has no Magento attributes'.format(product.name)))
-
-            for attrs in attributes_magento:
-                attr_magento = self.env['gt.product.attributes'].search([('magento_id', '=', attrs.attribute_id.attribute_magento_id)])
-                attribute_code = attr_magento.attribute_code
-                field_name = self.ATTRIBUTE_FIELD_MAPPING.get(attribute_code)
-                if field_name:
-                    vals[field_name] = vals.get(field_name, '') + attrs.name
+            if attributes_magento:
+                # Attributes exported from Magento
+                for attrs in attributes_magento:
+                    attr_magento = self.env['gt.product.attributes'].search([('magento_id', '=', attrs.attribute_id.attribute_magento_id)])
+                    attribute_code = attr_magento.attribute_code
+                    field_name = self.ATTRIBUTE_FIELD_MAPPING.get(attribute_code)
+                    if field_name:
+                        vals[field_name] = vals.get(field_name, '') + attrs.name
+            else:
+                # Attributes from Odoo
+                for attrs in product.product_template_attribute_value_ids:
+                    if attrs.display_type == 'color':
+                        if vals.get('ds_color_filter'):
+                            vals['ds_color_filter'] += attrs.name
+                        else:
+                            vals['ds_color_filter'] = attrs.name
 
             data.append(vals)
 
